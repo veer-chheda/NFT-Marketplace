d9d1df33c4d0721efbc36e6b07936246c483c031
veer-chheda <veerchheda3525@gmail.com>
2023-11-11 14:11:39 +0530

    added files

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config()
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Vf9yQxhWmYVH9dPDYh2it_sdZe08UpKn",
+      accounts: [ `${process.env.PRIVATE_KEY}` ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Marketplace from './components/Marketplace';
+import Profile from './components/Profile';
+import SellNFT from './components/SellNFT';
+import NFTPage from './components/NFTpage';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { useEffect } from 'react';
+
+function App() {
+  useEffect (() => {
+    document.title ="NFT Marketplace";
+  }, [])
+  return (
+    <div className="container">
+        <Routes>
+          <Route path="/" element={<Marketplace />}/>
+          <Route path="/nftPage" element={<NFTPage />}/>        
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sellNFT" element={<SellNFT />}/>             
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
